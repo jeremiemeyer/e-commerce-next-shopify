2fdcf53b0c279b28d19d6418a460ddadb14d01dd
Jeremie Meyer <jeremie.meyer357@gmail.com>
2024-02-26 14:30:38 +0100

    first commit

---
+++ e-commerce-store/app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard"
+import getProducts from "@/actions/get-products"
+import Container from "@/components/ui/container"
+import Billboard from "@/components/billboard"
+import ProductList from "@/components/product-list"
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard("22abce73-4416-4251-9738-1a4b82add1c8")
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default HomePage
